5d80660443dee7b3f0fe9da7befe5bce55716798
Hugo Hamon <hugo.hamon@sensio.com>
2012-03-24 13:13:01 +0100

    [HangmanBundle] fixed CSS and FileLoader object.

--- src/Sensio/Bundle/HangmanBundle/Loader/FileLoader.php
+++ src/Sensio/Bundle/HangmanBundle/Loader/FileLoader.php
@@ -10,7 +10,7 @@ abstract class FileLoader implements LoaderInterface
     public function __construct($file)
     {
         if (!file_exists($file) || !is_readable($file)) {
-            throw new \InvalidArgumentException(sprintf('File "%s" does not exist or is not readable.'));
+            throw new \InvalidArgumentException(sprintf('File "%s" does not exist or is not readable.', $file));
         }
 
         $this->file = $file;
